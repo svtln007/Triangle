3fbed27d3423f6cfa26593a1deb7c1950ae7121e
Светлана <@DESKTOP-IASTCHU>
2024-05-24 17:54:11 +0300

    Добавьте файлы проекта.

---
+++ Triangle/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Triangle
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Wrong number of arguments. Use: triangle.exe <num1> <num2> <num3>");
+                return;
+            }
+
+            if (double.TryParse(args[0], out double a) && double.TryParse(args[1], out double b) && double.TryParse(args[2], out double c))
+            {
+                string result = GetTriangleType(a, b, c);
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+        public static string GetTriangleType(double a, double b, double c)
+        {
+            if (a + b <= c || a + c <= b || b + c <= a)
+            {
+                return "Не треугольник";
+            }
+
+            else if (a == b && b == c)
+            {
+                return "Равносторонний";
+            }
+
+            else if (a == b || a == c || b == c)
+            {
+                return "Равнобедренный";
+            }
+
+            return "Обычный";
+        }
+    }
+}
---
+++ testTriangle/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace triangle_test
+{
+    class triangle_test
+    {
+        static void Main()
+        {
+            int testCounter = 1;
+            using var outputFile = new StreamWriter("../../../../output.txt", false);
+            using (var inputFile = new StreamReader("../../../../input.txt"))
+            {
+                string testCaseArguments;
+                string expectedOutput;
+                string realOutput;
+
+                while ((testCaseArguments = inputFile.ReadLine()) != null)
+                {
+                    char[] delimiter = { ' ' };
+                    string[] args = testCaseArguments.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
+                    expectedOutput = inputFile.ReadLine();
+
+                    var output = new StringWriter();
+                    Console.SetOut(output);
+                    Console.SetError(output);
+
+                    Triangle.Program.Main(args);
+                    realOutput = output.ToString();
+                    realOutput = realOutput.Replace("\n", "").Replace("\r", "");
+
+                    if (realOutput == expectedOutput)
+                    {
+                        outputFile.WriteLine("Test passed " + testCounter + ": successfully");
+                    }
+                    else
+                    {
+                        outputFile.WriteLine("Test " + testCounter + ": failed. Expected: " + expectedOutput + " Recieved: " + realOutput);
+                    }
+
+                    testCounter++;
+                }
+            }
+        }
+    }
+}
